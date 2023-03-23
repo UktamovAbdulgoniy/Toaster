785dedf85d74a5e26fd1821ba67523602bc56743
UktamovAbdulgoniy <104591179+UktamovAbdulgoniy@users.noreply.github.com>
2023-03-23 14:01:03 +0500

    Initial commit

---
+++ app/src/main/java/com/uktamov/toaster/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.uktamov.toaster
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        toast("auih9fj")
+    }
+}
